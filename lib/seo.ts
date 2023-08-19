3cb9e268f26c04a251f1dda10ec3de8f2831f13d
Lux Lu <luxlu0172@gmail.com>
2023-08-19 20:08:33 +0800

    modified title

--- lib/seo.ts
+++ lib/seo.ts
@@ -4,7 +4,7 @@ export const seo = {
     'An AI-assisted academic English Composition course tailored for sophomore students in SCUPI',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://hur-web.vercel.app'
       : 'http://localhost:3000'
   ),
 } as const
